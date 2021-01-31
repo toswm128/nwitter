74df290cc3cce72c4cfa326f184a7a996a8749f6
조민수 <63090043+toswm128@users.noreply.github.com>
2021-02-01 00:40:44 +0900

    firebase import

---
+++ nwitter/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDUflf9jtTW1RlfunGOek1zdCwHuXyZKBE",
+    authDomain: "nwiiter-e2a01.firebaseapp.com",
+    projectId: "nwiiter-e2a01",
+    storageBucket: "nwiiter-e2a01.appspot.com",
+    messagingSenderId: "358312510198",
+    appId: "1:358312510198:web:719a3645be4249426722d7"
+  };
+
+export default firebase.initializeApp(firebaseConfig);
