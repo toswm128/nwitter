b495007028b85469d521e2342d82f50cb056c67d
조민수 <63090043+toswm128@users.noreply.github.com>
2021-02-01 23:15:51 +0900

    Router setup

---
+++ nwitter/src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AppRouter from './Router';
+
+function App() {
+  return <div> <AppRouter /></div>
+}
+
+export default App;
---
+++ nwitter/src/components/Router.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import {HashRouter as Router, Route, Switch} from "react-router-dom"
+import Home from "../routes/Home";
+import Auth from "../routes/Auth";
+
+
+const AppRouter = () =>{
+    const [isLogggedIn, setIsLoggedIn] = useState(false);
+
+    return(
+    <Router>
+        <Switch>
+            {isLogggedIn ? (
+                <>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                </>
+            ):(
+                <Route exact path="/">
+                    <Auth />
+                </Route>
+            )}
+        </Switch>
+    </Router>
+    )
+};
+
+export default AppRouter;
